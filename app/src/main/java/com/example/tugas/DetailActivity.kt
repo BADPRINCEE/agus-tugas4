a46f98f00486ebe9c0005f31fd5660376a0ac79b
BADPRINCEE <118593207+BADPRINCEE@users.noreply.github.com>
2024-03-30 17:20:49 +0900

    AGUS LATIF HANAFI
    tugas-4

---
+++ app/src/main/java/com/example/tugas/DetailActivity.kt
@@ -0,0 +1,24 @@
+package com.example.tugas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tugas.databinding.ActivityDetailBinding
+import com.google.firebase.database.Exclude
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val int = intent
+        var name = int.getStringExtra("name")
+        var desc = int.getStringExtra("description")
+        var img = int.getStringExtra("imageUrl")
+
+        binding.imageTitle.text = name
+        binding.imageDesc.text = desc
+        img?.let { binding.imageDetail.loadImage (it) }
+    }
+    }
